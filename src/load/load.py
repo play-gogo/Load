e3ca3cdc16a1e3a604ac4192abb1f9f2ece123bc
hun0219 <gsw98000@gmail.com>
2024-08-05 18:34:10 +0900

    edit load

--- src/load/load.py
+++ src/load/load.py
@@ -6,7 +6,4 @@ def load(load_dt='20200101'):
     df = transform(load_dt)
     df.to_parquet("~/code/playgogo/storage", partition_cols=['month','load_dt'])
     
-    print(df)
-    #return df
-
 load()
